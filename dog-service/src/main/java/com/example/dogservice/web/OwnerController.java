philwebb/whats-new-in-spring-boot-3-0

Handle NoSuchDogOwner with ProblemDetail

dog-service/src/main/java/com/example/dogservice/web/OwnerController.java
@@ -2,7 +2,8 @@ package com.example.dogservice.web;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,8 +30,10 @@ public class OwnerController {
 	}
 
 	@ExceptionHandler
-	ResponseEntity<Void> onNoSuchDogOwner(NoSuchDogOwner ex) {
-		return ResponseEntity.notFound().build();
+	ProblemDetail onNoSuchDogOwner(NoSuchDogOwner ex) {
+		ProblemDetail details = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+		details.setProperty("ownername", ex.getName());
+		return details;
 	}
 
 }

hinyinlam/upgrading-to-spring-boot-3

dog-service/src/main/java/com/example/dogservice/web/OwnerController.java
@@ -2,7 +2,8 @@ package com.example.dogservice.web;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,8 +30,10 @@ public class OwnerController {
 	}
 
 	@ExceptionHandler
-	ResponseEntity<Void> onNoSuchDogOwner(NoSuchDogOwnerException ex) {
-		return ResponseEntity.notFound().build();
+	ProblemDetail onNoSuchDogOwner(NoSuchDogOwnerException ex) {
+		ProblemDetail details = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+		details.setProperty("ownername", ex.getName());
+		return details;
 	}
 
 }